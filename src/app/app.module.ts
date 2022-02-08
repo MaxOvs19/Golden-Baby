23afab5d5a99aa287ba0dc83007d65c2dd48ac0b
Max <Rock_maksimus@mail.ru>
2022-02-08 09:54:03 +0300

    the build first main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FirstMainPageComponent } from './first-main-page/first-main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FirstMainPageComponent
   ],
   imports: [
     BrowserModule,

ff2f02d8eccf94d2723cb5dba8eed5557cec1136
alexey_ovsianikov <aleksei.ovsianikov@noveogroup.com>
2022-05-15 15:48:46 +0300

    [TDF] Intro in data-binding

--- src/app/home/components/bid-form-introduction/bid-form-introduction.component.ts
+++ src/app/home/components/bid-form-introduction/bid-form-introduction.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IFeedback } from '../../interfaces/feedback.interface';
 
+//Angular works by pattern MVVM
 @Component({
   selector: 'app-bid-form-introduction',
-  templateUrl: './bid-form-introduction.component.html',
+  templateUrl: './bid-form-introduction.component.html', // View
   styleUrls: ['./bid-form-introduction.component.scss'],
 })
+// Component === ViewModel
 export class BidFormIntroductionComponent implements OnInit {
-  feedback: IFeedback = {
-    parentName: '',
+  @ViewChild('parentsName')
+  public first!: ElementRef;
+
+  // Model
+  public feedback: IFeedback = {
+    parentName: 'Lisa',
     phone: '',
     childName: '',
     birthday: new Date(),
     agreement: false,
   };
 
-  constructor() {}
+  public title = 'Hello!';
 
-  @ViewChild('parentsName')
-  first!: ElementRef;
-  ngAfterViewInit() {
-    console.log(this.first);
-  }
+  constructor() {}
 
   ngOnInit(): void {
     //console.log(this.feedback.parentName);
+
+    setTimeout(() => {
+      this.title = new Date().toString();
+      this.feedback.parentName = 'LOL';
+    }, 9000);
   }
 
-  onsubmid() {
+  submit(): void {
     if (this.feedback.parentName != '') {
       console.log(this.feedback.parentName);
-    } else console.log('Error: parentName');
+    } else {
+      console.log('Error: parentName');
+    }
+  }
+
+  test(pName: string): void {
+    console.log('pName: ', pName);
+    console.log('this.feedback: ', this.feedback);
   }
 }

0535615eeeb71f3bd607559a67c5dc40764a7b95
MaxOvs <rock_maksimus@mail.ru>
2022-08-10 23:17:54 +0300

    Add controll tab

---
+++ src/app/menu/interfaces/menu.interface.ts
@@ -0,0 +1,8 @@
+export interface Imenu {
+  dayWeek: string;
+  breakfast: string;
+  snack: string;
+  lunch: string;
+  afternoonTea: string;
+  dinner: string;
+}
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MenuRoutingModule } from './menu-routing.module';
 import { MenuComponent } from './menu.component';
 import { LinkReturnModule } from '../ui/link-return';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule, MenuRoutingModule, LinkReturnModule],
+  imports: [CommonModule, MenuRoutingModule, LinkReturnModule, MatTabsModule],
 })
 export class MenuModule {}

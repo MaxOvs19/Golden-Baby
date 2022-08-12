f7530dc3794c4762fe892a03a9da6b6f1c5f8712
Alexey Ovsyanikov <aleksei.ovsianikov@noveogroup.com>
2022-08-13 00:54:48 +0300

    [HTTP] Added HW

--- src/app/lessons/pages/http-requests/http-requests.component.ts
+++ src/app/lessons/pages/http-requests/http-requests.component.ts
@@ -4,6 +4,31 @@ import { pluck, tap } from 'rxjs';
 import { IUser } from './interfaces/user.interface';
 import { UsersService } from './services/users.service';
 
+/**
+ * HW
+ *
+ * 1. Получить пользователей из JSON файла, который находится в assets/data/users.json
+ *    и вывести результат с пощью mat-card
+ *
+ * 2. Добавить возможность получать пользователей на определенной странице
+ *    Пример: https://reqres.in/api/users?page=2
+ *    Реализовать с помощтю поля ввода:
+ *        (page: [1]) => https://reqres.in/api/users?page=1
+ *        (page: [2]) => https://reqres.in/api/users?page=2
+ *
+ * 3. Создать страницу для просмотра конкретного пользователя
+ *    https://reqres.in/api/users/1 =>
+ *
+ *    {
+        "id": 1,
+        "email": "george.bluth@reqres.in",
+        "first_name": "George",
+        "last_name": "Bluth",
+        "avatar": "https://reqres.in/img/faces/1-image.jpg"
+      }
+ *
+ */
+
 @Component({
   selector: 'app-http-requests',
   templateUrl: './http-requests.component.html',

f3d6e04f9651602c77d40969b7fcd6b3bdaf133d
MaxOvs19 <88626313+MaxOvs19@users.noreply.github.com>
2022-08-29 21:33:03 +0300

    Add feedback UI module (#25)
    * [dialog-feedback] Add mat-dialog in feedback
    
    * [dialog-feedback] Add changes feedback dialog
    
    * [dialog-feedback] Work form with dialog box
    
    * [FEAT] Fixed issues with dialog on the home page
    
    * [FEAT] Added TODO
    
    * [dialog-feedback] Ready to CR
    
    * [dialog-ui-feedback] Completed work
    
    Co-authored-by: MaxOvs <rock_maksimus@mail.ru>
    Co-authored-by: Alexey Ovsyanikov <aleksei.ovsianikov@noveogroup.com>

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from '@ui/button';
+import { FeedbackDialogModule } from '@ui/dialogs/feedback-dialog';
+import { FeedbackModule } from '@ui/feedback';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -9,13 +12,7 @@ import { ConditionsComponent } from './components/conditions/conditions.componen
 import { GroupsComponent } from './components/groups/groups.component';
 import { AdaptationChildrenComponent } from './components/adaptation-children/adaptation-children.component';
 import { BigQuotesComponent } from './components/big-quotes/big-quotes.component';
-import { FeedbackComponent } from './components/feedback/feedback.component';
-import { ButtonModule } from '../ui/button';
 import { SliderComponent } from './components/slider/slider.component';
-import { InputModule } from '../ui/input';
-import { DatePickerModule } from '../ui/date-picker';
-import { FeedbackDialogModule } from '../ui/dialogs/feedback-dialog';
-import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -24,7 +21,6 @@ import { MatDialogModule } from '@angular/material/dialog';
     GroupsComponent,
     AdaptationChildrenComponent,
     BigQuotesComponent,
-    FeedbackComponent,
     SliderComponent,
   ],
   imports: [
@@ -32,11 +28,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     HomeRoutingModule,
     ButtonModule,
     CarouselModule,
-    ReactiveFormsModule,
-    InputModule,
-    DatePickerModule,
     FeedbackDialogModule,
-    MatDialogModule,
+    FeedbackModule,
   ],
 })
 export class HomeModule {}

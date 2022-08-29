aa8ceecab54546b95920ad136993feb32fb83388
MaxOvs <rock_maksimus@mail.ru>
2022-08-29 16:59:56 +0300

    [dialog-feedback] Ready to CR

--- src/app/home/components/feedback/feedback.component.ts
+++ src/app/home/components/feedback/feedback.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { distinctUntilChanged, filter, startWith, switchMap, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 import { FeedbackDialogComponent } from '@ui/dialogs/feedback-dialog';
 
@@ -25,19 +24,6 @@ export class FeedbackComponent implements OnInit {
 
   public ngOnInit(): void {
     this.createForm();
-
-    // HW[RxJS]: switchMap, mergeMap, concatMap, exaustMap. Differences
-
-    // of(1, 2, 3, 4, 4, 5, 6, 6, 6, 6, 6, 6, 4)
-    //   .pipe(
-    //     // startWith('HR'), Start stream with 'HR' value
-    //     distinctUntilChanged(), // prev !== current
-    //     // distinctUntilChanged((prev, current) => prev !== current && current % 2 === 0),
-    //     tap((v) => {
-    //       console.log('v: ', v);
-    //     }),
-    //   )
-    //   .subscribe();
   }
 
   public control(name: string): AbstractControl | null {
@@ -59,24 +45,18 @@ export class FeedbackComponent implements OnInit {
         }),
         filter((result) => !!result),
         tap(() => {
-          // TODO: Need to find other solution for current route reloading
           window.location.reload();
         }),
       )
       .subscribe();
-
-    console.log(feedback);
   }
 
   private createForm(): void {
     this.feedbackForm = this._formBuilder.group({
-      parentName: ['проклу', [Validators.required, Validators.pattern(/^[а-я\s]+$/i)]],
-      childName: ['ауц', [Validators.required]],
+      parentName: ['', [Validators.required, Validators.pattern(/^[а-я\s]+$/i)]],
+      childName: ['', [Validators.required]],
       birthday: [new Date(), [Validators.required]],
-      phone: [
-        '+7 000 000 22 11',
-        [Validators.pattern(/^\+7\s\d{3}\s\d{3}\s\d{2}\s\d{2}$/), Validators.required],
-      ],
+      phone: ['', [Validators.pattern(/^\+7\s\d{3}\s\d{3}\s\d{2}\s\d{2}$/), Validators.required]],
       comment: [''],
       checkbox: [false, [Validators.requiredTrue]],
     });

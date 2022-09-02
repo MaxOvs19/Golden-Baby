426b0abb2a399956ae18f0584f134b9419224fde
MaxOvs <rock_maksimus@mail.ru>
2022-09-02 14:56:07 +0300

    [price-dialog] Tiket ready to CR

--- src/app/prices/prices.module.ts
+++ src/app/prices/prices.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from '@ui/button';
+import { LinkReturnModule } from '@ui/link-return';
 
 import { PricesRoutingModule } from './prices-routing.module';
 import { PricesComponent } from './prices.component';
-import { ButtonModule } from '../ui/button';
-import { LinkReturnModule } from '../ui/link-return';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FeedbackModule } from '@ui/feedback';
 
 @NgModule({
   declarations: [PricesComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     PricesRoutingModule,
     ButtonModule,
     LinkReturnModule,
     MatDialogModule,
-    FeedbackModule,
   ],
 })
 export class PricesModule {}
